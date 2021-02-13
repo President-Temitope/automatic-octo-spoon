82c3075134d6a73280eec0cdc8a0e1ca12e7541c
halphaman <abdulazeezyisau@gmIal.com>
2021-02-13 09:48:41 +0100

    first commit

---
+++ Modules/Investments/Resources/views/index.blade.php
@@ -0,0 +1,9 @@
+@extends('investments::layouts.master')
+
+@section('content')
+    <h1>Hello World</h1>
+
+    <p>
+        This view is loaded from module: {!! config('investments.name') !!}
+    </p>
+@endsection
