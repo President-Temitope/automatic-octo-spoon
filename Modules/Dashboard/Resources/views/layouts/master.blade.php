976c0c795b3785376606b3ae9be7c9e3a8302c80
president-temitope <abdulazeezyisau@gmail.com>
2021-03-30 04:59:57 +0100

    fixed tawk.to plugin

--- Modules/Dashboard/Resources/views/layouts/master.blade.php
+++ Modules/Dashboard/Resources/views/layouts/master.blade.php
@@ -443,7 +443,20 @@
 <!-- Page level custom scripts -->
 <script src="{{asset('js/demo/chart-area-demo.js')}}"></script>
 <script src="{{asset('js/demo/chart-pie-demo.js')}}"></script>
-
+    <!--Start of Tawk.to Script-->
+    <script type="text/javascript">
+        var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
+        (function(){
+            var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
+            s1.async=true;
+            s1.src='https://embed.tawk.to/6062a057067c2605c0bd8258/1f20k4uvp';
+            s1.charset='UTF-8';
+            s1.setAttribute('crossorigin','*');
+            s0.parentNode.insertBefore(s1,s0);
+        })();
+    </script>
+    <!--End of Tawk.to Script-->
+</div>
 </body>
 
 </html>
