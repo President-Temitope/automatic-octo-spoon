f98f9a72d4ea284c3a081eaa2e9728a77e7fe51a
president-temitope <abdulazeezyisau@gmail.com>
2021-04-05 23:45:34 +0100

    Settings features done

--- Modules/Core/Http/Controllers/CoreController.php
+++ Modules/Core/Http/Controllers/CoreController.php
@@ -5,75 +5,40 @@ namespace Modules\Core\Http\Controllers;
 use Illuminate\Contracts\Support\Renderable;
 use Illuminate\Http\Request;
 use Illuminate\Routing\Controller;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Validator;
+
 
 class CoreController extends Controller
 {
     /**
-     * Display a listing of the resource.
-     * @return Renderable
-     */
-    public function index()
-    {
-        return view('core::index');
-    }
-
-    /**
-     * Show the form for creating a new resource.
-     * @return Renderable
-     */
-    public function create()
-    {
-        return view('core::create');
-    }
-
-    /**
-     * Store a newly created resource in storage.
+     * Add settings table.
      * @param Request $request
      * @return Renderable
      */
-    public function store(Request $request)
+    public function addSettings(Request $request)
     {
-        //
-    }
+        $validator = Validator::make($request->all(),[
+            'bitcoinWalletAddress' => ['required','string','min:26','max:35'],
+            'litecoinWalletAddress' => ['required','string','min:26','max:35'],
+            'ethereumWalletAddress' => ['required','string','min:26','max:35']
+        ]);
+        if ($validator->failed()){
+            return redirect()->back()->withErrors($validator);
+        }
+        if (DB::table('settings')->where('id', 1)->exists()) {
+            DB::table('settings')->where('id', 1)->delete();
+        }
+        DB::table('settings')
+            ->insert(
+                [
+                    'id' => 1,
+                    'bitcoinWalletAddress'=> $request->bitcoinWalletAddress,
+                    'litecoinWalletAddress'=> $request->litecoinWalletAddress,
+                    'ethereumWalletAddress'=> $request->ethereumWalletAddress,
+                    ]
+            );
+        return redirect()->back()->with('success','Wallet addresses added successfully');
 
-    /**
-     * Show the specified resource.
-     * @param int $id
-     * @return Renderable
-     */
-    public function show($id)
-    {
-        return view('core::show');
-    }
-
-    /**
-     * Show the form for editing the specified resource.
-     * @param int $id
-     * @return Renderable
-     */
-    public function edit($id)
-    {
-        return view('core::edit');
-    }
-
-    /**
-     * Update the specified resource in storage.
-     * @param Request $request
-     * @param int $id
-     * @return Renderable
-     */
-    public function update(Request $request, $id)
-    {
-        //
-    }
-
-    /**
-     * Remove the specified resource from storage.
-     * @param int $id
-     * @return Renderable
-     */
-    public function destroy($id)
-    {
-        //
     }
 }
