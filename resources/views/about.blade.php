0d4a5fe2e490928b2636221185c2fcad2615f2d7
me <me@gmail.com>
2021-04-16 16:07:19 -0600

    new commit

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -8,20 +8,20 @@
             <div class="row about-content">
                 <!-- Image Starts -->
                 <div class="col-sm-12 col-md-5 col-lg-6 text-center">
-                    <img id="about-us" class="img-responsive img-about-us" src="images/about-us.png" alt="about us">
+                    <img id="about-us" class="img-responsive img-about-us" src="geg.jpeg" alt="about us">
                 </div>
                 <!-- Image Ends -->
                 <!-- Content Starts -->
                 <div class="col-sm-12 col-md-7 col-lg-6">
                     <div class="feature-about">
                         <h3 class="title-about">WE ARE {{config('app.name')}}</h3>
-                        <p>A place for everyone who wants to simply buy and sell Bitcoins. Deposit funds using your Visa/MasterCard or bank transfer. Instant buy/sell of Bitcoins at fair price is guaranteed. Nothing extra. Join over 700,000 users from all over the world satisfied with our services.</p>
+                        <p>A place for everyone who wants to simply mine and earn cryptocurrencies.sign up with  your wallet, choose most suitable mining planfor you with no hidden or required fees, automatic profit return guaranteed. Nothing extra. Join over 700,000 users from all over the world satisfied with our services.</p>
                     </div>
                     <div class="feature-about">
                         <h3 class="title-about risk-title"><i class="fa fa-warning"></i> risk warning</h3>
                         <p>Bitcoin is not legal tender and is not backed by any government. Accounts and value balances are not subject to any government backed deposit insurance or any other government protections.</p>
                     </div>
-                    <a class="btn btn-primary btn-services" href="/investments">Our plans</a>
+                    <a class="btn btn-primary btn-services" href="/investments">see plans</a>
                 </div>
                 <!-- Content Ends -->
 
@@ -37,8 +37,8 @@
             <!-- Fact Badges Starts -->
             <div class="row text-center facts-content">
                 <div class="text-center heading-facts">
-                    <h2>{{config('app.name')}}<span> numbers</span></h2>
-                    <p>leading cryptocurrency miner since day one of Bitcoin distribution</p>
+                    <h2>{{config('app.name')}}<span></span></h2>
+                    <p>leadind platform since the discovery of Bitcoin </p>
                 </div>
                 <!-- Fact Badge Item Starts -->
                 <div class="col-xs-12 col-md-3 col-sm-6 fact">
@@ -81,9 +81,7 @@
             <!-- Section Title Starts -->
             <div class="row text-center">
                 <h2 class="title-head">our <span>experts</span></h2>
-                <div class="title-head-subtitle">
-                    <p> A talented team of Cryptocurrency experts based in London</p>
-                </div>
+                <div class="title-head-subtitle">A TALENTED TEAM OF CRYPTOCURRENCY EXPERTS BASED IN AUSTRALIA</div>
             </div>
             <!-- Section Title Ends -->
             <!-- Team Members Starts -->
@@ -122,8 +120,8 @@
                         <!-- Team Member Picture Ends -->
                         <!-- Team Member Details Starts -->
                         <div class="team-member-caption social-icons">
-                            <h4>Marco Verratti</h4>
-                            <p>Director</p>
+                            <h4>JEFFERY KURT</h4>
+                            <p>founder and cmo</p>
                             <ul class="list list-inline social">
                                 <li>
                                     <a href="#" class="fa fa-facebook"></a>
@@ -145,12 +143,12 @@
                     <!-- Team Member-->
                     <div class="team-member">
                         <!-- Team Member Picture Starts -->
-                        <img src="images/team/member3.jpg" class="img-responsive" alt="team member">
+                        <img src="images/npk.jpeg" class="img-responsive" alt="team member">
                         <!-- Team Member Picture Ends -->
                         <!-- Team Member Details Starts -->
                         <div class="team-member-caption social-icons">
-                            <h4>Emilia Bella</h4>
-                            <p>Bitcoin Consultant</p>
+                            <h4>FREDDIE WADE</h4>
+                            <p>bitcoin consultant/admin</p>
                             <ul class="list list-inline social">
                                 <li>
                                     <a href="#" class="fa fa-facebook"></a>
@@ -171,12 +169,12 @@
                 <div class="col-xs-6 col-sm-3 col-md-3 col-lg-3">
                     <div class="team-member">
                         <!-- Team Member Picture Starts -->
-                        <img src="images/team/member4.jpg" class="img-responsive" alt="team member">
+                        <img src="images/nqe.jpeg" class="img-responsive" alt="team member">
                         <!-- Team Member Picture Ends -->
                         <!-- Team Member Details Starts -->
                         <div class="team-member-caption social-icons">
-                            <h4>Antonio Conte</h4>
-                            <p>Bitcoin Developer</p>
+                            <h4>ALFREDO KIRK</h4>
+                            <p>co founder</p>
                             <ul class="list list-inline social">
                                 <li>
                                     <a href="#" class="fa fa-facebook"></a>
@@ -207,7 +205,7 @@
                         <!-- Call To Action Text Starts -->
                         <div class="action-text">
                             <h2>Get Started Today With Mining</h2>
-                            <p class="lead">Open account for free and start mining Bitcoins!</p>
+                            <p class="lead">Open account for free and start mining cryptocurrencies!</p>
                         </div>
                         <!-- Call To Action Text Ends -->
                         <!-- Call To Action Button Starts -->
