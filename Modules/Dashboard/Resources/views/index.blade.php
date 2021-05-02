5ddffa1314809d1bdcc15dc078b40a4ca0fa3d14
president-temitope <abdulazeezyisau@gmail.com>
2021-05-02 15:50:25 +0100

    modal fix

--- Modules/Dashboard/Resources/views/index.blade.php
+++ Modules/Dashboard/Resources/views/index.blade.php
@@ -227,7 +227,7 @@
                                 <h3 class="text-capitalize text-center"><span>Plan hashrate  </span>{{$investment->rate}} </h3>
                                 @if(Auth::user()->hasRole('user'))
                                 <a href="#" data-toggle="modal" data-target="#{{$investment->id}}" class="text-center btn btn-outline-success">Make Offer</a>
-                                    <div class="modal fade" id="{{$investment->id}}">
+                                    <div class="modal fade" id="#{{$investment->id}}">
                                         <div class="modal-dialog" role="document">
                                             <div class="modal-content">
                                                 <div class="modal-header">
